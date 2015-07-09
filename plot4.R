7ab74a8203095d8f517d15077a9649d5fc67f261
estocking <estocking@users.noreply.github.com>
2015-07-09 11:57:57 -0500

    Update plot4.R
    streamline the graph creation

--- plot4.R
+++ plot4.R
@@ -22,18 +22,15 @@ data$Time <- strptime(data$Time, format="%H:%M:%S")
 ## Plot #4
 par(mfrow=c(2,2))
 
-# top left 
-plot(data$DateTime, data$Global_active_power, type="l", main="", xlab="", ylab="Global Active Power")
-# top right
-plot(data$DateTime, data$Voltage, type="l", main="", xlab="datetime", ylab="Voltage")
-# bottom left
-yrange <- range(c(data$Sub_metering_1, data$Sub_metering_2, data$Sub_metering_3))
-plot(data$DateTime, data$Sub_metering_1, ylim=yrange, col="black", type="l", xlab="", ylab="Energy sub metering")
-lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
-lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
-legend('topright', names(data)[7:9], lty=1, col=c('black', 'red', 'blue'), bty='n')
-# bottom right
-plot(data$DateTime, data$Global_reactive_power, type="l", main="", xlab="datetime", ylab="Global_reactive_power")
-
+# create graphs
+with(data, {
+  plot(DateTime, Global_active_power, type="l", main="", xlab="", ylab="Global Active Power")
+  plot(DateTime, Voltage, type="l", main="", xlab="datetime", ylab="Voltage")
+  plot(DateTime, Sub_metering_1, ylim=yrange, col="black", type="l", xlab="", ylab="Energy sub metering")
+    lines(DateTime, Sub_metering_2, type="l", col="red")
+    lines(DateTime, Sub_metering_3, type="l", col="blue")
+    legend('topright', names(data)[7:9], lty=1, col=c('black', 'red', 'blue'), bty='n')
+  plot(DateTime, Global_reactive_power, type="l", main="", xlab="datetime", ylab="Global_reactive_power")
+})
 dev.copy(png, "plot4.png", width=480, height=480)
 dev.off()
